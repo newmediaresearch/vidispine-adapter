0ce271352e96d6978f9839fa8749cc9e5392ff98
Laurence485 <31314787+Laurence485@users.noreply.github.com>
2020-09-10 15:50:07 +0100

    add delete collection (#9)
    * add delete collection
    
    * revisions
    
    * revisions
    
    * remove create_collection fixture from test_get
    
    * Update test_get.py

---
+++ tests/test_collections/test_delete.py
@@ -0,0 +1,21 @@
+import pytest
+
+from vidispine.errors import NotFound
+
+
+def test_delete(vidispine, cassette, create_collection):
+    vidispine.collection.delete(create_collection)
+
+    assert cassette.all_played
+
+
+def test_delete_not_found(vidispine, cassette):
+    with pytest.raises(NotFound) as err:
+        vidispine.collection.delete('VX-1000000')
+
+    assert cassette.all_played
+
+    err.match(
+        r'Endpoint not found: DELETE'
+        r' - http://localhost:8080/API/collection/VX-1000000'
+    )
