7ef27e0fd691ad1dd5909cf331226501b02d55f8
Laurence <laurencesrdunn@gmail.com>
2020-09-14 15:14:26 +0100

    add ability to reindex

---
+++ tests/test_collections/test_reindex.py
@@ -0,0 +1,14 @@
+def test_reindex_collection(vidispine, cassette):
+    index = 'collection'
+    result = vidispine.reindex(index)
+
+    assert result['index'] == index
+    assert cassette.all_played
+
+
+def test_reindex_item(vidispine, cassette):
+    index = 'item'
+    result = vidispine.reindex(index)
+
+    assert result['index'] == index
+    assert cassette.all_played
