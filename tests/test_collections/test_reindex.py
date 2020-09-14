affa093385e150775295073857118968b11c448e
Laurence <laurencesrdunn@gmail.com>
2020-09-14 15:32:49 +0100

    add not found test

--- tests/test_collections/test_reindex.py
+++ tests/test_collections/test_reindex.py
@@ -12,3 +12,11 @@ def test_reindex_item(vidispine, cassette):
 
     assert result['index'] == index
     assert cassette.all_played
+
+
+def test_reindex_index_not_found(vidispine, cassette):
+    index = 'foo'
+    result = vidispine.reindex(index)
+
+    assert result['index'] == index
+    assert cassette.all_played
