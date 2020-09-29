78c6316393865f1f5fe8d9962bad38c1de041f99
Laurence <laurencesrdunn@gmail.com>
2020-09-29 17:06:21 +0100

    initial commit

---
+++ vidispine/metadata.py
@@ -0,0 +1,15 @@
+from vidispine.typing import BaseJson
+
+
+class MetadataFieldGroup:
+
+    def __init__(self, client) -> None:
+        self.client = client
+
+    def get(self, field_group_name: str, params: dict = None) -> BaseJson:
+        if params is None:
+            params = {}
+
+        endpoint = f'metadata-field/field-group/{field_group_name}'
+
+        return self.client.get(endpoint, params=params)
