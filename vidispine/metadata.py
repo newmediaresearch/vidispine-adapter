1a019fed46a6fbeae5671ffc919f6d134afbc7fd
Laurence <31314787+Laurence485@users.noreply.github.com>
2020-09-30 17:37:31 +0100

    create metadata field group (#27)
    * initial commit
    
    * add create metadata field group
    
    * remove fixture

--- vidispine/metadata.py
+++ vidispine/metadata.py
@@ -7,6 +7,14 @@ class MetadataFieldGroup:
     def __init__(self, client) -> None:
         self.client = client
 
+    def create(self, field_group_name: str, params: dict = None) -> None:
+        if params is None:
+            params = {}
+
+        endpoint = f'metadata-field/field-group/{field_group_name}'
+
+        self.client.put(endpoint, params=params)
+
     def list(self, params: dict = None) -> BaseJson:
         if params is None:
             params = {}
