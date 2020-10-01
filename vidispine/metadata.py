abc79f8bcc2dc53d6b521cacdd31d4960e7c0f67
Laurence <31314787+Laurence485@users.noreply.github.com>
2020-10-01 12:21:58 +0100

    add create metadata field (#22)
    * initial commit
    
    * add create metadata field
    
    * add missing cassette assert
    
    * change var name
    
    * add metadata dict
    
    * add metadata class
    
    * revisions
    
    * add assert
    
    * fix assert

--- vidispine/metadata.py
+++ vidispine/metadata.py
@@ -29,6 +29,14 @@ class MetadataField:
     def __init__(self, client) -> None:
         self.client = client
 
+    def create(self, metadata: dict, field_name: str) -> BaseJson:
+        if not metadata:
+            raise InvalidInput('Please supply metadata.')
+
+        endpoint = f'metadata-field/{field_name}'
+
+        return self.client.put(endpoint, json=metadata)
+
     def get(
             self,
             field_name: str,
