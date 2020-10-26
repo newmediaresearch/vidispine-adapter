03eafa4e880dc60892107c2f1f766d8bdd6d08bb
Laurence <laurencesrdunn@gmail.com>
2020-10-26 15:56:20 +0000

    revisions

--- tests/test_search/test_search_with_search_doc.py
+++ tests/test_search/test_search_with_search_doc.py
@@ -4,54 +4,42 @@ import uuid
 import pytest
 
 from vidispine.errors import InvalidInput
-from vidispine.utils import generate_metadata
-
-
-@pytest.fixture
-def update_metadata(vidispine):
-    def _update_metadata(collection_or_item, vidispine_id):
-        endpoint = f'{collection_or_item}/{vidispine_id}/metadata'
-
-        fields = {
-            'title': 'foo_bar',
-            'field_one': 'pizza'
-        }
-
-        vidispine.client.request(
-            'put', endpoint, json=generate_metadata(fields)
-        )
-
-    return _update_metadata
 
 
 def test_search(
     vidispine,
     cassette,
-    create_metadata_field,
-    update_metadata,
     collection,
-    item
+    create_item
 ):
-    create_metadata_field('field_one')
-    update_metadata('collection', collection)
-    update_metadata('item', item)
+    if cassette.dirty:
+        item_uuid = str(uuid.uuid4())
+        create_item(item_uuid)
+    else:
+        create_item_request = json.loads(cassette.requests[1].body)
+
+        item_uuid = (
+            create_item_request['timespan'][0]['field'][0]['value'][0]['value']
+        )
 
     metadata = {
         'field': [
             {
-                'name': 'field_one',
-                'value': [{'value': 'pizza'}]
+                'name': 'title',
+                'value': [{'value': item_uuid}]
             }
         ]
     }
 
     result = vidispine.search(metadata)
 
-    assert result['hits'] > 2
+    assert result['hits'] == 1
     assert 'suggestion' in result
     assert 'autocomplete'
-    assert 'entry'
-    assert cassette.all_played
+    assert 'item' in result['entry'][0]
+
+    assert cassette.requests[2].method == 'PUT'
+    assert json.loads(cassette.requests[2].body) == metadata
 
 
 def test_search_with_params(
@@ -90,40 +78,45 @@ def test_search_with_params(
 
     check_field_value_exists(item_fields, 'title', item_uuid)
 
-    search_request_method = cassette.requests[2].method
-    assert search_request_method == 'PUT'
+    assert cassette.requests[2].method == 'PUT'
+    assert json.loads(cassette.requests[2].body) == metadata
 
 
 def test_search_with_matrix_params(
     vidispine,
     cassette,
-    create_metadata_field,
-    update_metadata,
     collection,
-    item
+    create_item
 ):
-    create_metadata_field('field_one')
-    update_metadata('collection', collection)
-    update_metadata('item', item)
+    if cassette.dirty:
+        item_uuid = str(uuid.uuid4())
+        create_item(item_uuid)
+    else:
+        create_item_request = json.loads(cassette.requests[1].body)
+
+        item_uuid = (
+            create_item_request['timespan'][0]['field'][0]['value'][0]['value']
+        )
 
     metadata = {
         'field': [
             {
-                'name': 'field_one',
-                'value': [{'value': 'pizza'}]
+                'name': 'title',
+                'value': [{'value': item_uuid}]
             }
         ]
     }
-
     result = vidispine.search(
         metadata=metadata, matrix_params={'number': 10, 'first': 1}
     )
 
-    assert result['hits'] > 2
+    assert result['hits'] == 1
     assert 'suggestion' in result
     assert 'autocomplete'
-    assert 'entry'
-    assert cassette.all_played
+    assert 'item' in result['entry'][0]
+
+    assert cassette.requests[2].method == 'PUT'
+    assert json.loads(cassette.requests[2].body) == metadata
 
 
 def test_search_with_params_and_matrix_params(
@@ -164,8 +157,8 @@ def test_search_with_params_and_matrix_params(
 
     check_field_value_exists(item_fields, 'title', item_uuid)
 
-    search_request_method = cassette.requests[2].method
-    assert search_request_method == 'PUT'
+    assert cassette.requests[2].method == 'PUT'
+    assert json.loads(cassette.requests[2].body) == metadata
 
 
 def test_search_invalid_input(vidispine):
