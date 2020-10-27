5b9b418d54e2bff14aaad3027dac320a2565f115
Laurence <laurencesrdunn@gmail.com>
2020-10-27 13:07:04 +0000

    add missing create_shape call

--- tests/test_search/test_search_shapes_without_search_doc.py
+++ tests/test_search/test_search_shapes_without_search_doc.py
@@ -1,18 +1,18 @@
 def test_search_shape(vidispine, cassette, item, create_shape):
+    create_shape(item)
+
     result = vidispine.search.shape()
 
-    # We cannot assert the exact shape id as Vidispine will sometimes
-    # reindex the shape id after creation.
     assert 'id' in result['shape'][0]
     assert result['shape'][0]['item'][0]['id'] == item
     assert cassette.all_played
 
 
 def test_search_shape_with_params(vidispine, cassette, item, create_shape):
+    create_shape(item)
+
     result = vidispine.search.shape(params={'content': 'metadata'})
 
-    # We cannot assert the exact shape id as Vidispine will sometimes
-    # reindex the shape id after creation.
     assert 'id' in result['shape'][0]
     assert result['shape'][0]['item'][0]['id'] == item
     assert cassette.all_played
@@ -21,10 +21,10 @@ def test_search_shape_with_params(vidispine, cassette, item, create_shape):
 def test_search_shape_with_matrix_params(
     vidispine, cassette, item, create_shape
 ):
+    create_shape(item)
+
     result = vidispine.search.shape(matrix_params={'number': 10, 'first': 1})
 
-    # We cannot assert the exact shape id as Vidispine will sometimes
-    # reindex the shape id after creation.
     assert 'id' in result['shape'][0]
     assert result['shape'][0]['item'][0]['id'] == item
     assert cassette.all_played
@@ -33,12 +33,13 @@ def test_search_shape_with_matrix_params(
 def test_search_shape_with_params_and_matrix_params(
     vidispine, cassette, item, create_shape
 ):
+
+    create_shape(item)
+
     result = vidispine.search.shape(
         params={'content': 'metadata'}, matrix_params={'number': 10}
     )
 
-    # We cannot assert the exact shape id as Vidispine will sometimes
-    # reindex the shape id after creation.
     assert 'id' in result['shape'][0]
     assert result['shape'][0]['item'][0]['id'] == item
     assert cassette.all_played
