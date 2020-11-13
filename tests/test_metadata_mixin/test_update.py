785dcc6d30af9dcf65a3b979901bfd8c7b6a2240
Laurence <laurencesrdunn@gmail.com>
2020-11-13 11:43:32 +0000

    add tests for item class

--- tests/test_metadata_mixin/test_update.py
+++ tests/test_metadata_mixin/test_update.py
@@ -66,8 +66,76 @@ def test_update_collection_collection_not_found(vidispine, cassette):
     assert cassette.all_played
 
 
-def test_update_invalid_input(vidispine):
+def test_update_collection_invalid_input(vidispine):
     with pytest.raises(InvalidInput) as err:
         vidispine.collection.update_metadata('VX-1000000', {})
+
+    err.match('Please supply metadata.')
+
+
+def test_update_item(
+    vidispine, cassette, create_metadata_field, item
+):
+
+    create_metadata_field('field_one')
+    create_metadata_field('field_two')
+
+    fields = {
+        'title': 'Foo bar',
+        'field_one': 'eggs',
+        'field_two': 123
+    }
+
+    vidispine.item.update_metadata(item, generate_metadata(fields))
+
+    assert cassette.all_played
+
+
+def test_update_item_field_does_not_exist(
+    vidispine, cassette, create_metadata_field, item
+):
+    create_metadata_field('field_one')
+    create_metadata_field('field_two')
+
+    fields = {
+        'title': 'Foo bar',
+        'field_one': 'eggs',
+        'field_two': 123,
+        'field_three': 'stuff'
+    }
+
+    with pytest.raises(NotFound) as err:
+        vidispine.item.update_metadata(
+            item, generate_metadata(fields)
+        )
+
+        re.match(
+            r'Endpoint not found: PUT'
+            r' - http://localhost:8080/API/item/VX-\d+$/metadata', err
+        )
+
+    assert cassette.all_played
+
+
+def test_update_item_item_not_found(vidispine, cassette):
+    fields = {
+        'title': 'Foo bar',
+        'field_one': 'eggs',
+        'field_two': 123
+    }
+
+    with pytest.raises(NotFound) as err:
+        vidispine.item.update_metadata(
+            'VX-1000000', generate_metadata(fields)
+        )
+
+    err.match(r'Not Found: PUT')
+
+    assert cassette.all_played
+
+
+def test_update_item_invalid_input(vidispine):
+    with pytest.raises(InvalidInput) as err:
+        vidispine.item.update_metadata('VX-1000000', {})
 
     err.match('Please supply metadata.')
