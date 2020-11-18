387faf77dca2ce88f935e9454154550515535f59
Laurence <laurencesrdunn@gmail.com>
2020-11-18 17:05:13 +0000

    add update storage

--- vidispine/storage.py
+++ vidispine/storage.py
@@ -1,7 +1,20 @@
-class Storage:
+from vidispine.base import EntityBase
+from vidispine.errors import InvalidInput
+from vidispine.typing import BaseJson
+
+
+class Storage(EntityBase):
     """Manages storages
 
     :vidispine_docs:`Vidispine doc reference <storage/storage>`
 
     """
     entity = 'storage'
+
+    def update(self, storage_id: str, metadata: dict) -> BaseJson:
+        if not metadata:
+            raise InvalidInput('Please supply metadata.')
+
+        endpoint = self._build_url(storage_id)
+
+        return self.client.put(endpoint, json=metadata)
