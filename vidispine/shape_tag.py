bc39fe71fa0f6b6d3da2f6286a9e75d662054079
Laurence Dunn <31314787+Laurence485@users.noreply.github.com>
2020-11-20 12:30:11 +0000

    add create or update shape tag (#63)
    * add create or update shape tag
    
    * remove test asserts

--- vidispine/shape_tag.py
+++ vidispine/shape_tag.py
@@ -1,4 +1,5 @@
 from vidispine.base import EntityBase
+from vidispine.errors import InvalidInput
 from vidispine.typing import BaseJson
 
 
@@ -11,6 +12,33 @@ class ShapeTag(EntityBase):
 
     """
     entity = 'shape-tag'
+
+    def create(self, shape_tag_name: str, metadata: dict) -> None:
+        """Creates a new shape tag with the given tag name.
+
+        :param shape_tag_name: The name of the shape tag to create.
+        :param metadata: The metadata (transcode preset document) to
+            create the shape tag with.
+
+        """
+        self._update(shape_tag_name, metadata)
+
+    def update(self, shape_tag_name: str, metadata: dict) -> None:
+        """Updates a shape tag with the given tag name.
+
+        :param shape_tag_name: The name of the shape tag to update.
+        :param metadata: The metadata (transcode preset document) to
+            update the shape tag with.
+
+        """
+        self._update(shape_tag_name, metadata)
+
+    def _update(self, shape_tag_name: str, metadata: dict) -> None:
+        if not metadata:
+            raise InvalidInput('Please supply metadata.')
+
+        endpoint = self._build_url(shape_tag_name)
+        self.client.put(endpoint, json=metadata)
 
     def list(self, params: dict = None) -> BaseJson:
         """Retrieves all shape tags known by the system.
