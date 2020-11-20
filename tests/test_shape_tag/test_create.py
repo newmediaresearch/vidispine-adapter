7308d83f38d7728fec91484a6650db8125fffa2c
Laurence <laurencesrdunn@gmail.com>
2020-11-20 11:52:00 +0000

    remove test asserts

--- tests/test_shape_tag/test_create.py
+++ tests/test_shape_tag/test_create.py
@@ -1,6 +1,3 @@
-import json
-from urllib.parse import urlparse
-
 import pytest
 
 from vidispine.errors import InvalidInput
@@ -9,12 +6,6 @@ from vidispine.errors import InvalidInput
 def test_create(vidispine, cassette, shape_tag_metadata):
     shape_tag_name = 'test'
     vidispine.shape_tag.create(shape_tag_name, shape_tag_metadata)
-
-    request = cassette.requests[0]
-
-    assert request.method == 'PUT'
-    assert json.loads(request.body) == shape_tag_metadata
-    assert urlparse(request.url).path == f'/API/shape-tag/{shape_tag_name}'
 
     assert cassette.all_played
 
